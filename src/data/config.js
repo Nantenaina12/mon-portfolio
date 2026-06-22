db92659bcef601d70de082f9dcbeedf8904770b7
Orlando <nantenainaorlando1@gmail.com>
2026-06-22 02:33:29 +0100

    Configuration de mon parcours

--- src/data/config.js
+++ src/data/config.js
@@ -1,8 +1,8 @@
 export const config = {
-  name: "Ton Nom",
-  title: "Développeur Full Stack",
-  tagline: "Je crée des expériences web modernes",
-  email: "ton.email@gmail.com",
-  github: "https://github.com/ton-utilisateur",
-  linkedin: "https://linkedin.com/in/ton-profil"
+  name: "ANDRINANTENAINA Alida Orlando",
+  title: " Eleve Ingénieur en SIG |Télédetection ",
+  tagline: "Je suis étudiant en deuxième année du cycle ingénieur de la filière Géoinformation ,passionné par les technologies Web SIG",
+  email: "nantenainaorlando1@gmail.com",
+  github: "https://github.com/Nantenaina12",
+  linkedin: "https://www.linkedin.com/in/orlando-nantenaina"
 }
